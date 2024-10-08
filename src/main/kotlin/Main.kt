bc53cd329c03581fe32f6f33ef85017c037b2d1b
Emmanuel Serna Yanes <emmanuel240158@gmail.com>
2024-10-07 23:42:53 -0500

    Stage 3/4:Need for speed. For sorting, the quick sort algorithm, and for searching, the binary search algorithm.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 package org.example
 
 import java.io.File
+import kotlin.math.min
+import kotlin.math.sqrt
 
 const val directoryFilePath = "src/main/kotlin/resources/small_directory.txt"
 const val phonebookFilePath = "src/main/kotlin/resources/small_find.txt"
@@ -27,6 +29,7 @@ interface Soart {
     fun sort(array: MutableList<Contact>)
 }
 
+
 object BubbleSort : Soart {
     override val name: String
         get() = "bubble sort"
@@ -45,12 +48,65 @@ object BubbleSort : Soart {
 
 }
 
+class QuickSort : Soart {
+    override val name: String
+        get() = "quick sort"
+
+    override fun sort(array: MutableList<Contact>) {
+        if (array.isNotEmpty()){
+            quickSort(array,0,array.size-1)
+        }
+    }
+
+    private fun quickSort(array: MutableList<Contact>, l: Int, r: Int) {
+        if (l < r) {
+            val p = partition(array, l, r)
+            quickSort(array, l, p - 1)
+            quickSort(array, p + 1, r)
+        }
+    }
+
+    private fun partition(array: MutableList<Contact>, l: Int, r: Int): Int {
+        val x = array[r]
+        var i = l - 1
+        for (j in l until r) {
+            if (array[j].name <= x.name) {
+                i++
+                val temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+            }
+        }
+        val temp = array[i + 1]
+        array[i + 1] = array[r]
+        array[r] = temp
+        return i + 1
+    }
+
+}
+
 object JumpSearch : Search {
     override val name: String
         get() = "jump search"
 
     override fun search(array: List<Contact>, name: String): Contact? {
-        return null
+        if (array.isEmpty()) return null
+        var curr = 1
+        var prev = 1
+        val last = array.size
+        val step = sqrt(last.toDouble()).toInt()
+
+        while (array[curr].name < name) {
+            if (curr == last) return null
+            prev = curr
+            curr = min(curr + step, last)
+        }
+
+        while (array[curr].name > name) {
+            if (--curr <= prev) return null
+        }
+
+        return if (array[curr].name == name) array[curr] else null
     }
 
 }
@@ -64,33 +120,72 @@ class LinearSearch : Search {
         array.find { it.name == name }
 }
 
+class BinarySearch : Search {
+    override val name: String
+        get() = "binary search"
+
+    override fun search(array: List<Contact>, name: String): Contact? {
+        return array.binarySearchBy(name)
+    }
+
+    private fun List<Contact>.binarySearchBy(name: String): Contact? {
+        return this.binarySearch { it.name.compareTo(name) }.takeIf { it >= 0 }?.let { this[it] }
+    }
+
+}
 
 fun main() {
     searchQueries(LinearSearch())
     println()
     searchQueries(JumpSearch, BubbleSort)
+    println()
+    searchQueries(BinarySearch(), QuickSort())
 }
 
-fun searchQueries(searchQuery: Search, soart: Soart? = null) {
+fun searchQueries(searchAlgorithm: Search, sortAlgorithm: Soart? = null) {
     val directoryEntries = File(directoryFilePath).readLines().map { Contact(it) }
     val phonebookEntries = File(phonebookFilePath).readLines()
 
-    if (soart != null) println("Start searching (${soart.name} + ${searchQuery.name})...") else println("Start searching (${searchQuery.name})...")
+    printSearchStartMessage(searchAlgorithm, sortAlgorithm)
+    val startTime = System.currentTimeMillis()
 
-    val start = System.currentTimeMillis()
-    soart?.sort(directoryEntries as MutableList<Contact>)
-    val sortTime = System.currentTimeMillis() - start
+    val sortedEntries = sortContacts(directoryEntries, sortAlgorithm)
+    val sortTime = elapsedTime(startTime)
 
-    val dataFind = phonebookEntries.count {
-        searchQuery.search(directoryEntries, it) != null
+    val foundEntriesCount = phonebookEntries.count { name ->
+        searchAlgorithm.search(sortedEntries, name) != null
     }
 
-    val totalTime = System.currentTimeMillis() - start
+    val totalTime = elapsedTime(startTime)
 
-    println("Found $dataFind / ${phonebookEntries.size} entries. Time taken: ${formatTime(totalTime)}")
-    if (soart != null) {
+    printSearchResults(phonebookEntries.size, foundEntriesCount, totalTime, sortTime)
+}
+
+fun elapsedTime(startTime: Long): Long =
+    System.currentTimeMillis() - startTime
+
+
+private fun sortContacts(contacts: List<Contact>, sortAlgorithm: Soart?): List<Contact> {
+    return sortAlgorithm?.let {
+        val mutableContacts = contacts.toMutableList()
+        it.sort(mutableContacts)
+        mutableContacts
+    } ?: contacts
+}
+
+private fun printSearchStartMessage(searchAlgorithm: Search, sortAlgorithm: Soart?) {
+    val message = if (sortAlgorithm != null) {
+        "Start searching (${sortAlgorithm.name} + ${searchAlgorithm.name})..."
+    } else {
+        "Start searching (${searchAlgorithm.name})..."
+    }
+    println(message)
+}
+
+private fun printSearchResults(totalEntries: Int, foundEntries: Int, totalTime: Long, sortTime: Long) {
+    println("Found $foundEntries / $totalEntries entries. Time taken: ${formatTime(totalTime)}")
+    if (sortTime != 0L) {
         println("Sorting time: ${formatTime(sortTime)}")
         println("Searching time: ${formatTime(totalTime - sortTime)}")
     }
-
 }
